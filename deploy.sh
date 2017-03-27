76564fb66e8c5025179c3ae192ee6393cabc14ae
benofben <ben.lackey@outlook.com>
2017-03-27 10:39:22 -0700

    something is wrong with my redirect

--- deploy.sh
+++ deploy.sh
@@ -6,27 +6,27 @@ REGION=`aws configure get region`
 
 COUCHBASE_USERNAME="couchbase"
 COUCHBASE_PASSWORD="foo1234"
-KEY="couchbase-$REGION"
+KEY="couchbase-${REGION}"
 
 echo "Getting SSH key..."
-KEY_FILENAME="~/.ssh/$KEY.pem"
-if [ ! -e $KEY_FILENAME ]
+KEY_FILENAME="~/.ssh/${KEY}.pem"
+if [ ! -e ${KEY_FILENAME} ]
 then
   echo "The key does not exist.  Generating a new key."
-  aws ec2 create-key-pair --region $REGION --key-name $KEY --query 'KeyMaterial' --output text > $KEY_FILENAME
-  chmod 600 $KEY_FILENAME
-  echo "Key saved to $KEY_FILENAME"
+  aws ec2 create-key-pair --region ${REGION} --key-name ${KEY} --query 'KeyMaterial' --output text > ${KEY_FILENAME}
+  chmod 600 ${KEY_FILENAME}
+  echo "Key saved to ${KEY_FILENAME}"
 fi
 
 echo "Validating template..."
-aws cloudformation validate-template --template-body $TEMPLATE_BODY
+aws cloudformation validate-template --template-body ${TEMPLATE_BODY}
 exit
 
 aws cloudformation create-stack \
---template-body $TEMPLATE_BODY \
---stack-name $STACK_NAME \
---region $REGION \
+--template-body ${TEMPLATE_BODY} \
+--stack-name ${STACK_NAME} \
+--region ${REGION} \
 --parameters \
-ParameterKey=Admin,ParameterValue=$COUCHBASE_USERNAME \
-ParameterKey=Password,ParameterValue=$COUCHBASE_PASSWORD \
-ParameterKey=KeyName,ParameterValue=$KEY
+ParameterKey=Admin,ParameterValue=${COUCHBASE_USERNAME} \
+ParameterKey=Password,ParameterValue=${COUCHBASE_PASSWORD} \
+ParameterKey=KeyName,ParameterValue=${KEY}
