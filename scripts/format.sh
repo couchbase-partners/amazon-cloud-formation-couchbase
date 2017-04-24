167fd40a910747646ae9a1cc47056482c6c0f92e
benofben <ben.lackey@outlook.com>
2017-04-24 15:54:29 -0700

    comment

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Need to figure out the ebs drive
+# Need to add an EBS drive to the CFn and configure it here.
 
 mkdir -p /datadisks/disk1
 chown -R couchbase /datadisks/disk1
