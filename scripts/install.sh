cc7b7225b59c238e045a0c82e787581cd327a022
benofben <ben.lackey@outlook.com>
2017-04-24 13:17:01 -0700

    unused

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,6 @@ echo "Running install.sh"
 
 # Using these instructions
 # https://developer.couchbase.com/documentation/server/4.6/install/rhel-suse-install-intro.html
-sudo yum install -y pkgconfig
 rpm --install couchbase-server-enterprise-4.6.1-centos7.x86_64.rpm
 
 #Warning: Transparent hugepages looks to be active and should not be.
