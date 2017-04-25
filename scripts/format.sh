78247bb58f172510d871b9d0cfe7960aae5a8275
benofben <ben.lackey@outlook.com>
2017-04-24 22:35:20 -0700

    maybe works

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-# Need to add an EBS drive to the CFn and configure it here.
-
 mkdir /mnt/datadisk
 chown couchbase /mnt/datadisk
 chgrp couchbase /mnt/datadisk
-
-#mount -a /dev/sdk /mnt/datadisk
+mount -a /dev/sdk /mnt/datadisk
