9528c1beb7ba33f6f40cf4d9a67d961e90944025
benofben <ben.lackey@outlook.com>
2017-04-24 22:14:25 -0700

    drop loop

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -50,12 +50,12 @@ output=""
 while [[ ! $output =~ "SUCCESS" ]]
 do
   output=`./couchbase-cli node-init \
-  --cluster=$nodePrivateDNS \
-  --node-init-hostname=$nodePrivateDNS \
-  --node-init-data-path=/datadisks/disk1/data \
-  --node-init-index-path=/datadisks/disk1/index \
-  --user=$adminUsername \
-  --pass=$adminPassword`
+    --cluster=$nodePrivateDNS \
+    --node-init-hostname=$nodePrivateDNS \
+    --node-init-data-path=/datadisks/disk1/data \
+    --node-init-index-path=/datadisks/disk1/index \
+    --user=$adminUsername \
+    --pass=$adminPassword`
   echo node-init output \'$output\'
   sleep 10
 done
@@ -67,32 +67,26 @@ then
   indexRAM=$((15 * $totalRAM / 100000))
 
   echo "Running couchbase-cli cluster-init"
-  output=""
-  while [[ ! $output =~ "SUCCESS" ]]
-  do
-    output=`./couchbase-cli cluster-init \
+  ./couchbase-cli cluster-init \
     --cluster=$nodePrivateDNS \
     --cluster-ramsize=$dataRAM \
     --cluster-index-ramsize=$indexRAM \
     --cluster-username=$adminUsername \
     --cluster-password=$adminPassword \
-    --services=data,index,query,fts`
-    echo cluster-init output \'$output\'
-    sleep 10
-  done
+    --services=data,index,query,fts
 else
   echo "Running couchbase-cli server-add"
   output=""
   while [[ $output != "Server $nodePrivateDNS:8091 added" && ! $output =~ "Node is already part of cluster." ]]
   do
     output=`./couchbase-cli server-add \
-    --cluster=$rallyPrivateDNS \
-    --user=$adminUsername \
-    --pass=$adminPassword \
-    --server-add=$nodePrivateDNS \
-    --server-add-username=$adminUsername \
-    --server-add-password=$adminPassword \
-    --services=data,index,query,fts`
+      --cluster=$rallyPrivateDNS \
+      --user=$adminUsername \
+      --pass=$adminPassword \
+      --server-add=$nodePrivateDNS \
+      --server-add-username=$adminUsername \
+      --server-add-password=$adminPassword \
+      --services=data,index,query,fts`
     echo server-add output \'$output\'
     sleep 10
   done
