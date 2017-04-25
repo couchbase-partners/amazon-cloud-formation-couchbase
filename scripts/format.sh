956ab3beae539d1ea137b373af2387a760052b05
benofben <ben.lackey@outlook.com>
2017-04-24 22:52:37 -0700

    clean up format

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
-mkdir /mnt/datadisk
-chown couchbase /mnt/datadisk
-chgrp couchbase /mnt/datadisk
-mount -a /dev/sdk /mnt/datadisk
+DEVICE=/dev/sdk
+MOUNTPOINT=/mnt/datadisk
+
+echo "Creating the filesystem."
+mkfs -t ext4 ${DEVICE}
+
+echo "Updating fstab"
+LINE="${DEVICE}\t${MOUNTPOINT}\text4\tdefaults,nofail\t0\t2"
+echo -e ${LINE} >> /etc/fstab
+
+echo "Mounting the disk"
+mkdir $MOUNTPOINT
+mount -a
+
+echo "Changing permissions"
+chown couchbase $MOUNTPOINT
+chgrp couchbase $MOUNTPOINT
