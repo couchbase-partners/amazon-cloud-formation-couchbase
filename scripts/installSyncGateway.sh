52a4c42554b08b7ffd5c74a8f0e3a3e0861ce4c1
benofben <ben.lackey@outlook.com>
2017-06-21 01:24:00 -0700

    install for redhat

--- scripts/installSyncGateway.sh
+++ scripts/installSyncGateway.sh
@@ -2,5 +2,5 @@
 
 echo "Running installSyncGateway.sh"
 
-wget https://packages.couchbase.com/releases/couchbase-sync-gateway/1.4.1/couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.deb
-dpkg -i couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.deb
+wget https://packages.couchbase.com/releases/couchbase-sync-gateway/1.4.1/couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.rpm
+rpm -i couchbase-sync-gateway-enterprise_1.4.1-3_x86_64.rpm
