c2c44a7692036a392f7a465e2931d2b0e0ce270b
benofben <ben.lackey@outlook.com>
2017-06-20 23:34:01 -0700

    params changed

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -4,12 +4,13 @@ TEMPLATE_BODY="file://couchbase.template"
 STACK_NAME=$1
 REGION=`aws configure get region`
 
+ServerInstanceCount="4"
+ServerDiskSize="100"
+SyncGatewayInstanceCount="2"
+INSTANCE_TYPE="m4.xlarge"
 USERNAME="couchbase"
 PASSWORD="foo123!"
 KEY_NAME="couchbase-${REGION}"
-INSTANCE_TYPE="m4.xlarge"
-INSTANCE_COUNT="4"
-DISK_SIZE="100"
 
 KEY_FILENAME=~/.ssh/${KEY_NAME}.pem
 if [ ! -e ${KEY_FILENAME} ]
@@ -26,9 +27,10 @@ aws cloudformation create-stack \
 --stack-name ${STACK_NAME} \
 --region ${REGION} \
 --parameters \
+ParameterKey=ServerInstanceCount,ParameterValue=${ServerInstanceCount} \
+ParameterKey=ServerDiskSize,ParameterValue=${ServerDiskSize} \
+ParameterKey=SyncGatewayInstanceCount,ParameterValue=${SyncGatewayInstanceCount} \
+ParameterKey=InstanceType,ParameterValue=${INSTANCE_TYPE} \
 ParameterKey=Username,ParameterValue=${USERNAME} \
 ParameterKey=Password,ParameterValue=${PASSWORD} \
-ParameterKey=KeyName,ParameterValue=${KEY_NAME} \
-ParameterKey=InstanceType,ParameterValue=${INSTANCE_TYPE} \
-ParameterKey=InstanceCount,ParameterValue=${INSTANCE_COUNT} \
-ParameterKey=DiskSize,ParameterValue=${DISK_SIZE}
+ParameterKey=KeyName,ParameterValue=${KEY_NAME}
