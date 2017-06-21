a490fbede7123c7bbce2d9c8fe9eaa483b634853
benofben <ben.lackey@outlook.com>
2017-06-20 23:59:44 -0700

    not going to work but nice placeholder anyway

--- scripts/configureSyncGateway.sh
+++ scripts/configureSyncGateway.sh
@@ -2,7 +2,35 @@
 
 echo "Running configureSyncGateway.sh"
 
-serverDNS=""
+region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.region'  \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+instanceID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
+  | jq '.instanceId' \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+autoscalingGroup=$(aws ec2 describe-instances \
+  --region ${region} \
+  --instance-ids ${instanceID} \
+  | jq '.Reservations[0]|.Instances[0]|.Tags[] | select( .Key == "aws:autoscaling:groupName") | .Value' \
+  | sed 's/^"\(.*\)"$/\1/' )
+
+autoscalingGroupInstanceIDs=$(aws autoscaling describe-auto-scaling-groups \
+  --region ${region} \
+  --query 'AutoScalingGroups[*].Instances[*].InstanceId' \
+  --auto-scaling-group-name ${autoscalingGroup} \
+  | grep "i-" | sed 's/ //g' | sed 's/"//g' |sed 's/,//g' | sort)
+
+rallyInstanceID=`echo ${autoscalingGroupInstanceIDs} | cut -d " " -f1`
+
+rallyPublicDNS=$(aws ec2 describe-instances \
+    --region ${region} \
+    --query  'Reservations[0].Instances[0].NetworkInterfaces[0].Association.PublicDnsName' \
+    --instance-ids ${rallyInstanceID} \
+    --output text)
+
+serverDNS=${rallyPublicDNS}
 
 echo '
 {
