aec9b90edd3ca16fae8ecf98bf3bbfd84449c0e7
benofben <ben.lackey@outlook.com>
2017-08-17 13:46:25 -0700

    pass more stack name vars

--- scripts/server.sh
+++ scripts/server.sh
@@ -4,11 +4,13 @@ echo "Running server.sh"
 
 adminUsername=$1
 adminPassword=$2
+stackName=$3
 
 echo "Using the settings:"
 echo adminUsername \'$adminUsername\'
 echo adminPassword \'$adminPassword\'
+echo stackName \'$stackName\'
 
 ./configureOS.sh
 ./format.sh
-./configureServer.sh $adminUsername $adminPassword
+./configureServer.sh $adminUsername $adminPassword $stackName
--- scripts/syncGateway.sh
+++ scripts/syncGateway.sh
@@ -3,8 +3,10 @@
 echo "Running server.sh"
 
 serverAutoScalingGroup=$1
+stackName=$2
 
 echo "Using the settings:"
 echo serverAutoScalingGroup \'$serverAutoScalingGroup\'
+echo stackName \'$stackName\'
 
-./configureSyncGateway.sh $serverAutoScalingGroup
+./configureSyncGateway.sh $serverAutoScalingGroup $stackName
