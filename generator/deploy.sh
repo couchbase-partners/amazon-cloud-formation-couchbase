58540eeb2ceb0fda671c2631afe30ff059691d18
benofben <ben.lackey@outlook.com>
2017-08-24 19:28:19 -0700

    skeleton fixed

--- generator/deploy.sh
+++ generator/deploy.sh
@@ -6,23 +6,23 @@ STACK_NAME=$2
 # create generatedTemplate.json
 python deployment.py parameters.${PARAMETERS_FILE}.yaml
 
-TEMPLATE_BODY="file://generated.template"
-REGION=`aws configure get region`
+#TEMPLATE_BODY="file://generated.template"
+#REGION=`aws configure get region`
 
-KEY_NAME="couchbase-${REGION}"
-KEY_FILENAME=~/.ssh/${KEY_NAME}.pem
-if [ ! -e ${KEY_FILENAME} ]
-then
-  echo "The key does not exist.  Generating a new key."
-  aws ec2 create-key-pair --region ${REGION} --key-name ${KEY_NAME} --query 'KeyMaterial' --output text > ${KEY_FILENAME}
-  chmod 600 ${KEY_FILENAME}
-  echo "Key saved to ${KEY_FILENAME}"
-fi
+#KEY_NAME="couchbase-${REGION}"
+#KEY_FILENAME=~/.ssh/${KEY_NAME}.pem
+#if [ ! -e ${KEY_FILENAME} ]
+#then
+#  echo "The key does not exist.  Generating a new key."
+#  aws ec2 create-key-pair --region ${REGION} --key-name ${KEY_NAME} --query 'KeyMaterial' --output text > ${KEY_FILENAME}
+#  chmod 600 ${KEY_FILENAME}
+#  echo "Key saved to ${KEY_FILENAME}"
+#fi
 
-aws cloudformation create-stack \
---capabilities CAPABILITY_IAM \
---template-body ${TEMPLATE_BODY} \
---stack-name ${STACK_NAME} \
---region ${REGION} \
---parameters \
-ParameterKey=KeyName,ParameterValue=${KEY_NAME}
+#aws cloudformation create-stack \
+#--capabilities CAPABILITY_IAM \
+#--template-body ${TEMPLATE_BODY} \
+#--stack-name ${STACK_NAME} \
+#--region ${REGION} \
+#--parameters \
+#ParameterKey=KeyName,ParameterValue=${KEY_NAME}
--- generator/deployment.py
+++ generator/deployment.py
@@ -5,28 +5,36 @@ import json
 def main():
     filename=sys.argv[1]
     print('Using parameter file: ' + filename)
-
     with open(filename, 'r') as stream:
         parameters = yaml.load(stream)
-
     print('Parameters: ' + str(parameters))
 
     template={
+        "AWSTemplateFormatVersion": "2010-09-09",
+        "Description": "AWS Deployment for Couchbase Enterprise",
+        "Parameters": {
+            "KeyName": {
+                "Description": "Name of an existing EC2 KeyPair",
+                "Type": "AWS::EC2::KeyPair::KeyName"
+            }
+        },
+        "Mappings": {},
+        "Resources": {}
     }
 
     username = parameters['username']
     password = parameters['password']
     cluster = parameters['cluster']
-    template['resources']+=generateCluster(cluster)
+    template['Resources'] = dict(template['Resources'].items() + generateCluster(cluster).items())
 
     file = open('generated.template', 'w')
     file.write(json.dumps(template, sort_keys=True, indent=4, separators=(',', ': ')) + '\n')
     file.close()
 
 def generateCluster(cluster):
-    resources = []
+    resources = {}
     for group in cluster:
-        resources+=generateGroup(group)
+        resources = dict(resources.items() + generateGroup(group).items())
     return resources
 
 def generateGroup(group):
@@ -36,7 +44,7 @@ def generateGroup(group):
     diskSize = group['diskSize']
     services = group['services']
 
-    resources={}
+    resources = {}
     return resources
 
 main()
