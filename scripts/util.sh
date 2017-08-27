28b903eb37baca1a14afa36f69f719b4270ea863
benofben <ben.lackey@outlook.com>
2017-08-26 18:07:55 -0700

    bad param numbers

--- scripts/util.sh
+++ scripts/util.sh
@@ -2,14 +2,13 @@
 
 getRallyPublicDNS ()
 {
-  adminUsername=$1
-  adminPassword=$2
-  stackName=$3
+  stackName=$1
 
   region=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
     | jq '.region'  \
     | sed 's/^"\(.*\)"$/\1/' )
 
+  # if no rallyAutoscalingGroup was passed then the node this is running on is part of the rallyAutoscalingGroup
   if [ -z "$2" ]
   then
     instanceID=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document \
@@ -22,7 +21,7 @@ getRallyPublicDNS ()
         | jq '.Reservations[0]|.Instances[0]|.Tags[] | select( .Key == "aws:autoscaling:groupName") | .Value' \
         | sed 's/^"\(.*\)"$/\1/' )
   else
-    rallyAutoScalingGroup=$4
+    rallyAutoScalingGroup=$2
   fi
 
   rallyAutoscalingGroupInstanceIDs=$(aws autoscaling describe-auto-scaling-groups \
