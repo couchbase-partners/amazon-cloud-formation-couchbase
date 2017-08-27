7ca9aec36b72e3313cf8641ffa333dc66c7c0076
benofben <ben.lackey@outlook.com>
2017-08-26 18:51:03 -0700

    clean up ident

--- scripts/util.sh
+++ scripts/util.sh
@@ -67,20 +67,20 @@ turnOffTransparentHugepages ()
   # Please look at http://bit.ly/1ZAcLjD as for how to PERMANENTLY alter this setting.
 
   echo "#!/bin/bash
-  ### BEGIN INIT INFO
-  # Provides:          disable-thp
-  # Required-Start:    $local_fs
-  # Required-Stop:
-  # X-Start-Before:    couchbase-server
-  # Default-Start:     2 3 4 5
-  # Default-Stop:      0 1 6
-  # Short-Description: Disable THP
-  # Description:       disables Transparent Huge Pages (THP) on boot
-  ### END INIT INFO
+### BEGIN INIT INFO
+# Provides:          disable-thp
+# Required-Start:    $local_fs
+# Required-Stop:
+# X-Start-Before:    couchbase-server
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Disable THP
+# Description:       disables Transparent Huge Pages (THP) on boot
+### END INIT INFO
 
-  echo 'never' > /sys/kernel/mm/transparent_hugepage/enabled
-  echo 'never' > /sys/kernel/mm/transparent_hugepage/defrag
-  " > /etc/init.d/disable-thp
+echo 'never' > /sys/kernel/mm/transparent_hugepage/enabled
+echo 'never' > /sys/kernel/mm/transparent_hugepage/defrag
+" > /etc/init.d/disable-thp
   chmod 755 /etc/init.d/disable-thp
   service disable-thp start
   chkconfig disable-thp on
@@ -92,6 +92,6 @@ setSwappinessToZero ()
 
   sysctl vm.swappiness=0
   echo "
-  # Required for Couchbase
-  vm.swappiness = 0" >> /etc/sysctl.conf
+# Required for Couchbase
+vm.swappiness = 0" >> /etc/sysctl.conf
 }
