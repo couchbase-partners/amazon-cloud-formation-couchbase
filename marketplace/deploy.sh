a72b103f8260504eb6ddced9a1866891f21770c9
Ben Lackey <ben.lackey@outlook.com>
2018-04-11 13:40:47 -0700

    cut deploy down

--- marketplace/deploy.sh
+++ marketplace/deploy.sh
@@ -2,13 +2,9 @@
 
 STACK_NAME=$1
 
-TEMPLATE_BODY="file://261a9b6d-4be2-40b3-bb5f-80930a4a9570.458010ec-a1ef-41de-b2bc-377eaf74f1c5.template"
+TEMPLATE_BODY="file://byol.template"
 REGION=`aws configure get region`
 
-ServerInstanceCount="4"
-ServerDiskSize="100"
-SyncGatewayInstanceCount="2"
-InstanceType="m4.xlarge"
 Username="couchbase"
 Password="foo123!"
 KeyName="couchbase-${REGION}"
@@ -20,10 +16,6 @@ aws cloudformation create-stack \
 --stack-name ${STACK_NAME} \
 --region ${REGION} \
 --parameters \
-ParameterKey=ServerInstanceCount,ParameterValue=${ServerInstanceCount} \
-ParameterKey=ServerDiskSize,ParameterValue=${ServerDiskSize} \
-ParameterKey=SyncGatewayInstanceCount,ParameterValue=${SyncGatewayInstanceCount} \
-ParameterKey=InstanceType,ParameterValue=${InstanceType} \
 ParameterKey=Username,ParameterValue=${Username} \
 ParameterKey=Password,ParameterValue=${Password} \
 ParameterKey=KeyName,ParameterValue=${KeyName} \
