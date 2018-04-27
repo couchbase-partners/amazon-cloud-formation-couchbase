d899c280d65027cbed4483242eab5fa05a2c47ab
Ben Lackey <ben.lackey@outlook.com>
2018-04-27 06:56:08 -0700

    move to 2.0.0

--- marketplace/installSyncGateway.sh
+++ marketplace/installSyncGateway.sh
@@ -1,4 +1,4 @@
 echo "Installing Couchbase Sync Gateway..."
-version=1.5.1
+version=2.0.0
 wget https://packages.couchbase.com/releases/couchbase-sync-gateway/${version}/couchbase-sync-gateway-enterprise_${version}_x86_64.rpm
 rpm --install couchbase-sync-gateway-enterprise_${version}_x86_64.rpm
