ebfd4636ab75fb30f95ca50bccc9c3648865974a
Shomari Liburd <shomari.liburd@couchbase.com>
2020-01-20 15:50:30 -0800

    Added 6.5.0 for server and 2.7.0 for SGW Removing BYOL6 for the mappings.  3 server and 1 sgw as default.

--- simple/deploy.sh
+++ simple/deploy.sh
@@ -7,13 +7,13 @@ REGION=`aws configure get region`
 
 ServerInstanceCount="3"
 ServerDiskSize="100"
-SyncGatewayInstanceCount="0"
+SyncGatewayInstanceCount="1"
 InstanceType="m5.xlarge"
 Username="couchbase"
 Password="foo123!"
 KeyName="couchbase-${REGION}"
 License=BYOL #BYOL or HourlyPricing
-LicenseSelect=BYOL6 #BYOL or HourlyPricing for lower than server 6.0.0.  For greater than 6.0.0 or to use Amazon Linux 2 BYOL6 or HourlyPricing6
+LicenseSelect=BYOL #BYOL or HourlyPricing for lower than server 6.0.0.  For greater than 6.0.0 or to use Amazon Linux 2 BYOL6 or HourlyPricing6
 
 
 aws cloudformation create-stack \
