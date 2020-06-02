dfa13478355ebd4283186c53412b747804baec5d
Shomari Liburd <shomari.liburd@couchbase.com>
2020-06-02 12:17:42 -0700

    -Corrected broken deploy-60plus.sh as it had a wrong reference to Licenses. - Updated to latest versions.

--- simple/deploy-60plus.sh
+++ simple/deploy-60plus.sh
@@ -9,20 +9,20 @@ REGION=`aws configure get region`
 Username="couchbase" #For the Couchbase Web Console
 Password="foo123!" #For the Couchbase Web Console
 KeyName="couchbase-${REGION}" #The ssh key that will be used to connect to the nodes
+License=BYOL #Couchbase Server license use: BYOL or HourlyPricing
 
 #Couchbase Server Settings
 InstanceType="m5.xlarge" #Couchbase Server Instance Type
 ServerInstanceCount="3"
 ServerDiskSize="100"
-ServerVersion="6.5.0"
+ServerVersion="6.5.1"
 Services="data" #seperate each service with \\, e.g data\\,index\\,query\\,fts\\,eventing\\,analytics
-ServerLicense=HourlyPricing #Couchbase Server license use: ANNUAL or HourlyPricing
+
 
 #Couchbase Sync Gateway Settings
-SyncGatewayVersion="2.7.1"
+SyncGatewayVersion="2.7.3"
 SyncGatewayInstanceCount="0"
 SyncGatewayInstanceType="m5.large"
-SGWLicense=BYOL #Couchbase Sync Gateway license use: BYOL or HourlyPricing
 
 aws cloudformation create-stack \
 --capabilities CAPABILITY_IAM \
